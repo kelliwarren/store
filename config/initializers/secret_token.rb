d780a9bc3f524aec67347e3cc3155108a945a7f4
kelliwarren <kmdesigngroup@gmail.com>
2014-02-19 09:35:09 -0800

    made rails scaffold for products, customers, categories

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'de1535d0b56eab1762768bb4c86051897711a261f9bfea450e9d03b6e34977ce56e1cc7b48c19dd949a68c5db5037ec9b2032c4f3f461e2083c81da96a70f5ad'
