0bcde8ca4d798b6bb1356181137c208570e45c58
kelliwarren <kmdesigngroup@gmail.com>
2014-02-19 12:18:38 -0800

    made faker files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "ffaker"
+
+def drop_all_the_things
+  puts 'DROP ALL THE THINGS!'
+  [Product, Category, Customer].map(&:destroy_all)
+end
+
+def set_up_development_database
+  200.times do |i|
+    products = Product.create(
+      {
+        company: Faker::Company.name,
+        website: Faker::Internet.uri('http'), 
+        upc: Faker::Identification.drivers_license,
+        name: Faker::BaconIpsum.words.join(' '),
+        price: rand(5..278),
+        description: Faker::BaconIpsum.paragraph,
+
+      }
+      )
+  end
+
+
+  30.times do |i|
+    customers = Customer.create(
+    {
+      name: Faker::BaconIpsum.words.join(' '),
+      last_name: Faker::BaconIpsum.words.join(' '),
+      email: Faker::Internet.email,
+      contact_phone: Faker::PhoneNumber.short_phone_number,
+      street: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::AddressUS.state,
+      zip: Faker::AddressUS.zip_code,
+    }
+     )
+  end
+
+  @categories = ["Women's Apparel", "Men's Apparel", "Home", "Children's Apparel", "Automotive", "Grocery", "Footwear"]
+  20.times do |i|
+    categories = Category.create(
+    {
+      name: @categories.sample,
+      description: Faker::BaconIpsum.paragraph,
+    }
+      )
+  end
+end
+
+case Rails.env
+  when 'development'
+    drop_all_the_things
+    set_up_development_database
+  when 'production'
+    #do nothing
+end
